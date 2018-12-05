f59c047758e5875d2944ebe5be744b9f403c34ef
jlshaw117 <jlshaw117@gmail.com>
2018-12-05 14:18:02 -0800

    add guest view

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :gifts, only:[:index, :show]
+    resources :gifts, only: [:index, :show]
+    resources :guests, only: [:index, :show]
 end
 end
