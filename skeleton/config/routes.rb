e83a3a927f5e3de48fe3ac02c7aefb151ba2dfd6
jlshaw117 <jlshaw117@gmail.com>
2018-12-05 14:28:05 -0800

    add Gifts views

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :gifts, only: [:index, :show]
-    resources :guests, only: [:index, :show]
+    resources :gifts, only: [:show]
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index]
+    end
 end
 end
