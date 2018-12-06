745fc54d0f30404d3a781a33cb753f65375df793
jlshaw117 <jlshaw117@gmail.com>
2018-12-05 17:32:40 -0800

    final commit for day

--- Jbuilder, React, Redux/frontend/pokedex.jsx
+++ Jbuilder, React, Redux/frontend/pokedex.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from './util/api_util';
 import { RECEIVE_ALL_POKEMON, receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, root);
 
-  //testing 
+  //testing
+  window.store = store;
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = APIUtil.fetchAlPokemon;
 });
